e620513b6ba5ca13e3826554060a4ec3f7aeee22
Gonzo <edwin.gonzales.n@gmail.com>
2018-02-27 16:54:46 -0600

    Control flow exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,13 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
+        int i = 0;
 
-        while (i <= 15){
-            System.out.print(" " + i++);
-        }
-
-
+//        while (i <= 15){
+//            System.out.print(" " + i++);
+//        }
+        do {
+            System.out.print(i);
+            i+=2;
+        } while(i<102);
     }
 }
