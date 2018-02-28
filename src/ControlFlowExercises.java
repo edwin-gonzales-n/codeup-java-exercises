1ecdb793afdee62eb08f5fae855151344fc83011
Gonzo <edwin.gonzales.n@gmail.com>
2018-02-28 00:48:52 -0600

    adding more  exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int i = 0;
@@ -9,12 +10,23 @@ public class ControlFlowExercises {
 //            System.out.print(i);
 //            i+=2;
 //        } while(i<102);
-        int  i = 100;
-        do {
-            System.out.printf("%d ", i);
-            i-=5;
-        } while(i >= -10);
-
+//        int  i = 100;
+//        do {
+//            System.out.printf("%d ", i);
+//            i-=5;
+//        } while(i >= -10);
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
+        int i = 1;
+        do{
+            System.out.println((int) Math.pow(i,2));
+            i*=2;
+        }while(i <= 500);
 
     }
 }
