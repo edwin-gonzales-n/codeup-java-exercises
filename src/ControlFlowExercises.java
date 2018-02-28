a72eb5f6915821bf24f8fa5fcee7d47856ac8310
Gonzo <edwin.gonzales.n@gmail.com>
2018-02-27 22:37:20 -0600

    adding 3rd exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,20 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
+//        int i = 0;
 
 //        while (i <= 15){
 //            System.out.print(" " + i++);
 //        }
+//        do {
+//            System.out.print(i);
+//            i+=2;
+//        } while(i<102);
+        int  i = 100;
         do {
-            System.out.print(i);
-            i+=2;
-        } while(i<102);
+            System.out.printf("%d ", i);
+            i-=5;
+        } while(i >= -10);
+
+
     }
 }
