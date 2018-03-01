7166c9a923ca49d923bcab0b7d93de387853c2fd
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-01 11:41:50 -0600

    Adding the counter where the user can only guess up to 10 times

--- src/HighLow.java
+++ src/HighLow.java
@@ -12,16 +12,25 @@ public class HighLow {
 //        Hints
 //        Use the random method of the java.lang.Math class to generate a random number.
         Scanner sc = new Scanner(System.in);
-        int userInput;
+        Random rand = new Random();
+        int  n = rand.nextInt(100) + 1;
+        int userGuesses = 0;
         String userExit;
         // System calls for user
         System.out.println("\nHello, and welcome to the 'Guess the Number' game\n" +
                 "The number is between 1 and 100 so good luck!\n" +
+                "YOU HAVE UP TO 10 GUESSES TO GET IT RIGHT!" +
                 "Brought to you by the creators of DJ Gonzo Productions!\n");
         System.out.println("Would you like to play ?");
         userExit = sc.next();
         while(userExit.equals("yes") || userExit.equals("y")) {
-            letsPlay();
+            letsPlay(n);
+            userGuesses++;
+            System.out.println("Guesses: " + userGuesses);
+            if(userGuesses == 10){
+                System.out.println("GAME OVER KID!!!");
+                System.exit(0);
+            }
         }
         if(userExit.equals("no") || userExit.equals("n")){
             System.out.println("Thank you for playing!!  Bye!\n");
@@ -29,28 +38,24 @@ public class HighLow {
         }
     }
     // functions go here
-    public static void letsPlay(){
-        Random rand = new Random();
-        int  n = rand.nextInt(100) + 1;
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("I got a number, guess what number it is: ");
-        int userInput = sc.nextInt();
-        if (userInput == n) {
-            System.out.println("WOW...GOOD GUESS!!\n");
-            System.out.println("Would you like to play again?");
-            String userExit = sc.next();
-                    if(userExit.equals("yes") || userExit.equals("y")){
-                        letsPlay();
-                    } else if(userExit.equals("n") || userExit.equals("no")){
-                        System.out.println("Thank you for playing!!\n");
-                        System.exit(0);
-                    }
-        } else if(userInput < n){
-            System.out.println("Nope....HIGHER!");
-        } else if(userInput > n){
-            System.out.println("Nope....LOWER!");
-        } else System.exit(0);
-    }
-
+        public static void letsPlay(int n){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("I got a number, guess what number it is: ");
+            int userInput = sc.nextInt();
+            if (userInput == n) {
+                System.out.println("WOW...GOOD GUESS!!\n");
+                System.out.println("Would you like to play again?");
+                String userExit = sc.next();
+                        if(userExit.equals("yes") || userExit.equals("y")){
+                            letsPlay(n);
+                        } else if(userExit.equals("n") || userExit.equals("no")){
+                            System.out.println("Thank you for playing!!\n");
+                            System.exit(0);
+                        }
+            } else if(userInput < n){
+                System.out.println("Nope....HIGHER!");
+            } else if(userInput > n){
+                System.out.println("Nope....LOWER!");
+            } else System.exit(0);
+        }
 }
