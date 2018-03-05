226146ef926a423d5dc975680a56dd83a0f7a624
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-04 22:15:02 -0600

    Adding execise 1 on arrays

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,13 @@
 public class ServerNameGenerator {
+    String[] adjectives = {"attractive", "bald", "beautiful", "chubby", "clean", "dazzling", "drab", "elegant", "fancy", "fit"};
+    String[] nouns = {"man", "mountain", "state", "ocean", "country", "building", "cat", "airline","love", "wealth"};
+
+    public int random(){
+        return (int) Math.floor(Math.random()* 9);
+    }
+    public static void main(String[] args) {
+        ServerNameGenerator random = new ServerNameGenerator();
+        System.out.println("Here is your server name:");
+        System.out.printf("%s-%s", random.adjectives[random.random()], random.nouns[random.random()]);
+    }
 }
