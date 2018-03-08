40a5dfe3c89158cf1e20163243eb1a947aef9b88
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-08 17:49:08 -0600

    Adding some comments for better readibility

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -19,25 +19,26 @@ public class GradesApplication {
         Student student4 = new Student("Bob");
         student4.addGrade(98);  student4.addGrade(98); student4.addGrade(98); student4.addGrade(100);
 
-        HashMap students = new HashMap<String, Integer>();
-        students.put("ED_TheMonsta",student1.getGradeAverage());
-        students.put("JoeBoy1994",student2.getGradeAverage());
-        students.put("GonzoInTexas",student3.getGradeAverage());
-        students.put("Bob_ElLoco",student4.getGradeAverage());
+        HashMap students = new HashMap<String, Integer>(); // HashMap Initializing
+        students.put("ED_TheMonsta",student1.getGradeAverage()); //Assigning gitHub username + super class average data
+        students.put("JoeBoy1994",student2.getGradeAverage()); //Assigning gitHub username + super class average data
+        students.put("GonzoInTexas",student3.getGradeAverage()); //Assigning gitHub username + super class average data
+        students.put("Bob_ElLoco",student4.getGradeAverage()); //Assigning gitHub username + super class average data
 
-        Set<String> keys = students.keySet();
-        TreeSet<String> sortedKeys = new TreeSet<>(keys);
+        Set<String> keys = students.keySet(); //get keys from students Hashmap
+        TreeSet<String> sortedKeys = new TreeSet<>(keys); // sorted logic to display usernames in order.
 
-        do{
+        do{ // do while starts as true and repeats until user types exit.
             System.out.println("\nWelcome\n" +
                     "Here are the github usernames of our students:");
-            for(String key : sortedKeys)
-                System.out.printf("|%s| ", key );
+            for(String key : sortedKeys) // for each loop to get data from the keyset
+                System.out.printf("|%s| ", key ); //  displaying names as asked in exercise
 
+            // App begins asking user to select a user
             System.out.println("\nWhat student would you like to see more information on?  Enter 'exit' to leave program.");
             userInput = input.next();
-            for(String key : sortedKeys)
-                if(key.equalsIgnoreCase(userInput)){
+            for(String key : sortedKeys) // for each to extract keys' data
+                if(key.equalsIgnoreCase(userInput)){ //  if userInput contains data in any of keys then display
                     System.out.println("The average grade of "+ key + " is " + students.get(key) + ". ");
                     if(userInput.equalsIgnoreCase("ED_TheMonsta")){
                         System.out.println("Name: " + student1.getName() + ". Total grades: " + student1.getGrades());
@@ -52,11 +53,9 @@ public class GradesApplication {
                         System.out.println("Name: " + student4.getName() + ". Total grades: " + student4.getGrades());
                         break;
                     }
-                } else if(userInput.equalsIgnoreCase("exit")){
+                } else if(userInput.equalsIgnoreCase("exit")){  // exit program upon typing 'exit'
                     System.exit(0);
                 }
         } while(true);
-
-
-    }
-}
+    } // end of main
+} // end of function
