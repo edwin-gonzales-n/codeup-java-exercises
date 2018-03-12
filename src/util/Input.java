486e49fedf87dfd611675c22546a49f74869a755
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-12 02:17:01 -0500

    adding exceptions exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,27 +7,39 @@ public class Input {
     public Input (){
         scanner = new Scanner(System.in);
     }
+
     public String getString(){
         return scanner.nextLine();
     }
+
     public boolean yesNo(){
         String userInput = getString();
         if(userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y")){
             return true;
         } else return false;
     }
+
     public boolean yesNo(String prompt){
         System.out.println(prompt);
         return yesNo();
     }
     // int
     public int getInt(){
-        return scanner.nextInt();
+        try{
+            return Integer.valueOf(scanner.next());
+        }catch (NumberFormatException NumberFormatException){
+            System.err.println("Exception " + NumberFormatException);
+            scanner.nextLine();
+            System.out.println("Input must be a whole number:");
+            return getInt();
+        }
     }
+
     public int getInt(String prompt){
         System.out.println(prompt);
         return getInt();
     }
+
     public int getInt(int min, int max){
         int userInput = getInt();
         if(userInput < min || userInput > max){
@@ -36,18 +48,28 @@ public class Input {
         } else
             return userInput;
     }
+
     public int getInt(int min, int max, String prompt){
         System.out.println(prompt);
         return getInt(min,max);
     }
     // double
     public double getDouble(){
-        return scanner.nextDouble();
+        try{
+            return Double.valueOf(scanner.next());
+        }catch (NumberFormatException NumberFormatException){
+            System.err.println("Exception " + NumberFormatException);
+            scanner.nextLine();
+            System.out.println("Input must be a 'double' (decimal) number:");
+            return getDouble();
+        }
     }
+
     public double getDouble(String prompt){
         System.out.println(prompt);
         return getDouble();
     }
+
     public double getDouble(double min, double max){
         double userInput = getDouble();
         if(userInput < min || userInput > max){
@@ -59,4 +81,29 @@ public class Input {
         System.out.println(prompt);
         return getDouble(min,max);
     }
+
+    public int getBinary(){
+        try{
+            return Integer.valueOf(scanner.next(), 2);
+        }catch (NumberFormatException NumberFormatException){
+            System.err.println("Exception " + NumberFormatException);
+            scanner.nextLine();
+            System.out.println("Input must be a binary number:");
+            return getBinary();
+        }
+    }
+
+    public int getHex(){
+        try{
+            return Integer.valueOf(scanner.next(), 16);
+        }catch (NumberFormatException NumberFormatException){
+            System.err.println("Exception " + NumberFormatException);
+            scanner.nextLine();
+            System.out.println("Input must be a Hex number:");
+            return getBinary();
+        }
+    }
+
+
+
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,16 +1,33 @@
 package util;
 
+import java.util.SortedMap;
+
 public class InputTest {
     public static void main(String[] args) {
         Input in = new Input();
 
-        boolean string1 = in.yesNo("Hello would you like to run this app?");
-        if (string1){
-            int num1 = in.getInt(1,10,"Please enter a number between 1 and 10:");
-            System.out.println("You entered the number " + "'" + num1 + "'");
-            double double1 = in.getDouble(1.0,100.0, "Please enter a decimal from 1.0 to 100.0");
-            System.out.println("You entered the decimal " + "'" + double1 + "'");
-        } else
-            System.exit(0);
+//        boolean string1 = in.yesNo("Hello would you like to run this app?");
+//        if (string1){
+//            int num1 = in.getInt(1,10,"Please enter a number between 1 and 10:");
+//            System.out.println("You entered the number " + "'" + num1 + "'");
+//            double double1 = in.getDouble(1.0,100.0, "Please enter a decimal from 1.0 to 100.0");
+//            System.out.println("You entered the decimal " + "'" + double1 + "'");
+//        } else
+//            System.exit(0);
+
+        System.out.println("Please enter a number: ");
+        int userInput = in.getInt();
+        System.out.println("You entered: " + userInput);
+
+        System.out.println("Please enter a decimal: ");
+        double userDouble = in.getDouble();
+        System.out.println("You entered: " + userDouble);
+
+        System.out.println("Please enter a binary number: ");
+        System.out.println("You entered: "+ in.getBinary());
+
+        System.out.println("Please enter a Hex number: ");
+        System.out.println("You entered: "+ in.getHex());
+
     }
 }
