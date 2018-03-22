a1148655d350a47607875433a1513d59402e0a13
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-22 00:27:56 -0500

    adding some self exercises

---
+++ src/MaximunFiinder/MaximumFinderTest.java
@@ -0,0 +1,8 @@
+package MaximunFiinder;
+
+public class MaximumFinderTest {
+    public static void main(String[] args) {
+        MaximumFinder finder = new MaximumFinder();
+        finder.determineMaximun();
+    }
+}
---
+++ src/SixSidedRoll/RandomIntegers.java
@@ -0,0 +1,18 @@
+package SixSidedRoll;
+
+import java.util.Random;
+
+public class RandomIntegers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int face;
+
+        for (int counter = 1; counter <= 20; counter++){
+            face = 1 + random.nextInt(6);
+            System.out.printf("%d ", face);
+
+            if (counter % 5 == 0)
+                System.out.println();
+        }
+    }
+}
